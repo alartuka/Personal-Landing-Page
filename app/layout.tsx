789bbaf55eb65acfdc4751e094789014a44c74de
alartuka <tukaalarbi@gmail.com>
2024-07-29 02:51:48 -0400

    update 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
-const inter = Inter({ subsets: ["latin"] });
+
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Tuka Alsharief | Tuka Alarbi",
-  description: "Tuka Alsharief - fka Tuka Alarbi - 's website showcasing her work",
+  description: "Tuka Alsharief - fka Tuka Alarbi -'s website showcasing her work and passion for Software engineering and AI",
 };
 
 export default function RootLayout({
@@ -19,15 +20,30 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/Logo.ico" sizes="lg"/>
+        <link rel="icon" href="/Logo.ico" sizes="lg" />
+
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          async
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}></Script>
+
+        <Script id="ga-script" strategy="lazyOnload">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.GOOGLE_ANALYTICS}');`}
+        </Script>
       </head>
+
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
-          disableTransitionOnChange
-        >
+          disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
