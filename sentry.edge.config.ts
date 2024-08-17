3ea96117a39533a8fb61598ff885abf11f5e4db2
alartuka <tukaalarbi@gmail.com>
2024-08-16 20:34:41 -0400

    update 1.2

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,14 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+// import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://6f28d4ad2c480541da8f86d499e4989e@o4507682842411008.ingest.us.sentry.io/4507682855321600",
+// Sentry.init({
+//   dsn: "https://6f28d4ad2c480541da8f86d499e4989e@o4507682842411008.ingest.us.sentry.io/4507682855321600",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+//   // Adjust this value in production, or use tracesSampler for greater control
+//   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+//   // Setting this option to true will print useful information to the console while you're setting up Sentry.
+//   debug: false,
+// });
