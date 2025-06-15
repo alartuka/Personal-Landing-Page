93f86d87f7fee6a4942da2fa55fd4750239a589c
Tuka Alsharief <80016785+alartuka@users.noreply.github.com>
2025-06-15 01:00:37 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,6 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
-import CC from "./CC";
 
 const Footer = () => {
   
@@ -43,7 +42,43 @@ const Footer = () => {
 
       </div>
 
-      <CC />
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+        Copyright &copy; 2024 Tuka Alsharief 
+        
+{/*         <span className="text-purple">• </span> Inspired by{" "}
+        
+        <Link href="https://github.com/adrianhajdin" target="_blank" rel="noopener noreferrer" color="inherit">Adrian Hajdin</Link> */}
+      </p>
+      
+
+      <div className="flex items-center md:gap-3 gap-6">
+        {socialMedia.map((info) => (
+          <Link
+            key={`link=${info.id}`}
+            href={info.link}
+            className={cn(
+              "w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            )}
+          >
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.7 }}
+              onHoverStart={(e) => {}}
+              onHoverEnd={(e) => {}}
+            >
+              <Image
+                src={info.img}
+                alt="Tuka Alsharief | Tuka Alarbi"
+                width={20}
+                height={20}
+              />
+            </motion.div>
+          </Link>
+        ))}
+      </div>
+    </div>
+      
     </footer>
   );
 };
